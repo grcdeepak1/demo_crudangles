fd4a1d2123caa6054132e9a49b2c27f10a334270
Michael Alexander <alexandermw@gmail.com>
2015-08-14 17:13:03 -0700

    second screencast through starting controllers

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'angular_rails_csrf'
 group :development, :test do
 
   gem 'rspec-rails'
+  gem 'factory_girl'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'angular#index'
 
+  scope :api do
+    scope :v1 do
+      resources :posts
+      resources :comments, only: [:create, :update, :destroy]
+    end
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
